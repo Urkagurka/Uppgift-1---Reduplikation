891b5e4d95cd3c784a990b1d2491ea88cb965c79
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-11-24 09:17:48 +0100

    Add project files.

---
+++ Uppgift 1 - Reduplikation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket ord?");
+            string ord = Console.ReadLine();
+            Console.WriteLine("Hur många upprepningar?");
+            int antal = int.Parse(Console.ReadLine());
+            for (int i = 0; i < antal; i++)
+            {
+                Console.Write(ord);
+            }
+
+        }
+
+    }
+}
+    
